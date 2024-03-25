89830d80c93c121c6e8b1e1792262dd094984c37
M.Owais Abdullah <owaisgold12@gmail.com>
2024-03-26 00:42:47 +0500

    Number Guessing Game added

---
+++ Number_Guess_Game/app.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 11);
+const answer = await inquirer.prompt([{
+        name: "userguessnum",
+        type: "number",
+        message: "Please guess a number between 1 - 10:",
+    }]);
+if (answer.userguessnum === randomnumber) {
+    console.log("Congratulation you guessed the right number.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
